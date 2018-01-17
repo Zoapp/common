ae29c612899d3670419e962f33b16db674abaac4
mikbry <mik@opla.ai>
2018-01-17 16:45:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zoapp-core &middot; [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/zoapp/core/blob/master/LICENSE) [![Build Status](https://travis-ci.org/Zoapp/common.svg?branch=master)](https://travis-ci.org/Zoapp/common)
+Javascript ES7 common framework for Zoapp client / frontend apps
---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import WebService from "./services/webService";
+import AuthService from "./services/authService";
+import SocketService from "./services/socketService";
+import EventEmitter from "./utils/eventEmitter";
+
+export { AuthService, WebService, SocketService, EventEmitter };
+
